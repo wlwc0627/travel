a3fe72a17ad19001f5583f1d96ad3f54d48ac895
wlwc0627 <33441607+actorWen@users.noreply.github.com>
2018-12-20 13:48:41 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="icon-wrapper">
+    <swiper :options="swiperOption">
+        <swiper-slide v-for='(page, index) of pages' :key='index'>
+          <div class="icon" v-for="item of page" :key="item.id">
+            <img class="icon-img" :src="item.imgUrl" />
+            <p class="icon-desc">{{item.desc}}</p>
+          </div>
+        </swiper-slide>
+        <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      iconList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/8c/47630407f70e8302.png',
+        desc: '影视基地'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+        desc: '宁波必游'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/ab/6f7d6e44963c9302.png',
+        desc: '泡温泉'
+      }, {
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+        desc: '一日游'
+      }, {
+        id: '0006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/8c/47630407f70e8302.png',
+        desc: '横店影视城'
+      }, {
+        id: '0007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/57/1e29afd06f881102.png',
+        desc: '普陀山'
+      }, {
+        id: '0008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b8/c5dcdb58deec2402.png',
+        desc: '象山影视城'
+      }, {
+        id: '0009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/f6/e54fad3ea337b02.gif',
+        desc: '年终大促'
+      }, {
+        id: '0010',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/80/416c6ab3368d1f02.png',
+        desc: '全部玩乐'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+  @import '~styles/mixins.styl'
+  .icon-wrapper >>> .swiper-container
+    height: 0
+    padding-bottom: 50%
+  .icon
+    width: 25%
+    height: 0
+    padding-bottom: 25%
+    float: left
+    text-align: center
+    .icon-img
+      width: 1.1rem
+      padding-top: .12rem
+    .icon-desc
+      font-size: .28rem
+      color: #616162
+</style>
